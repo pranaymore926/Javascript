2e4dbdd165e76c526e0714c3f1c4d7ecfacc3460
Pranay More <53988027+pranaymore926@users.noreply.github.com>
2024-08-01 12:20:09 +0000

    new file

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,23 @@
+console.log("Basic Program")
+
+// we will not use var for variables//
+
+const accountID = 12382727467
+var emailId = 'pranay@google.com'
+accountType = " Savings"
+
+//DataTypes
+//String
+let name = "Pranay"
+// Integer
+let age = 18
+// boolean
+let isLoggedIn = true
+
+// below is undefined variable as value is not assigned
+let state
+ 
+//null is empty value assigned
+let address = null
+
+console.log(typeof age);
