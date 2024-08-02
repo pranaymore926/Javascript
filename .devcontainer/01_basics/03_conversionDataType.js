9d419fc7e0ca18986fbaa8a4a3bfdc8873ae4f0f
Pranay More <53988027+pranaymore926@users.noreply.github.com>
2024-08-02 03:17:20 +0000

    conversion and comparision

--- .devcontainer/01_basics/03_conversionDataType.js
+++ .devcontainer/01_basics/03_conversionDataType.js
@@ -1,8 +1,21 @@
 let score = "100"
 
-console.log(typeof score);
+//console.log(typeof score);
 
 // conversion of string to Number
 
 let marks = Number(score)
-console.log(typeof marks);
+//console.log(typeof marks);
+
+//***************************Operations*****************************************
+
+let x = 6;
+//postfix
+y = x++
+console.log(y , x);
+
+let a = 7;
+//prefix
+b = ++a
+console.log(b , a);
+
---
+++ .devcontainer/01_basics/04_comparisons.js
@@ -0,0 +1,13 @@
+// console.log(null >= 0);
+// console.log(null === 0 );
+// console.log(null == 0)
+// console.log(null < 0);
+// console.log(null <=  0);
+
+console.log(undefined == 0);
+
+
+// === is strict check 
+
+console.log("2" == 2); // answer is true 
+console.log("2" === 2);// answer is false
